3c1fbe726bfccbca8f3941183833c478a160520d
Red Daly <reddaly@gmail.com>
2024-10-04 08:01:35 -0700

    Slightly update DeepImport test.

--- gazelle/kotlin/tests/deep_import/src/main/java/com/example/deepimport/DeepImport.kt
+++ gazelle/kotlin/tests/deep_import/src/main/java/com/example/deepimport/DeepImport.kt
@@ -1,13 +1,13 @@
 package com.example.deepimport
 
-import com.google.common.pretend.deep.Thing
-import com.google.common.pretend.deep.Things
+import com.google.common.base.pretend.deep.Thing
+import com.google.common.base.pretend.deep.Things as ThingsHelper
 
 /** This application compares two numbers, using the Ints.compare method from Guava. */
 class DeepCompare() {
     companion object {
         fun compare(a : Thing, b: Thing) {
-            return Things.compare(a, b)
+            return ThingsHelper.compare(a, b)
         }
     }
 }
